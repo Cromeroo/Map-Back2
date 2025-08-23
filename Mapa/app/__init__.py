4560f92944a3a25e10dffb7f82c29a1a9a8e1acb
Cromeroo <cromeroo@unal.edu.co>
2025-08-23 17:17:43 -0500

    cambio22

--- Mapa/app/__init__.py
+++ Mapa/app/__init__.py
@@ -36,7 +36,12 @@ def create_app():
 
     from .routes.auth_routes import auth_routes
     app.register_blueprint(auth_routes, url_prefix="/auth")
-
+    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+        "pool_size": 2,          # Mantén solo 2 conexiones abiertas en el pool
+        "max_overflow": 1,       # Permite como máximo 1 conexión extra temporal
+        "pool_recycle": 280,     # Recicla conexiones inactivas
+        "pool_pre_ping": True    # Verifica la conexión antes de usarla
+    }
     # --- SOLUCIÓN 1 (LA MÁS IMPORTANTE) ---
     # Esta función se ejecutará después de cada petición para cerrar la sesión
     # y devolver la conexión a la base de datos al pool.
